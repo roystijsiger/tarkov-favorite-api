388353442070b443e5382534fc13d114df5bf633
Roy-delcom <roy@delcom.nl>
2020-06-06 19:50:33 +0200

    Come on bruv look at connecitonstring :(

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://localhost/nest'),
+      mongoose.connect(process.env.CONNECTION_STRING  ?? 'mongodb://localhost/nest'),
   },
 ];
